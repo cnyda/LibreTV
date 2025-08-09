f6d6a76d8ece5d964d08e55b6f3a28775319f90d
cnyda <70762823+cnyda@users.noreply.github.com>
2025-08-09 23:24:19 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -115,38 +115,37 @@ const API_SITES = {
         name: '量子资源站'
     },
     testSource: {
-        api: 'https://www.example.com/api.php/provide/vod',
-        name: '空内容测试源',
-        adult: true
+        api: 'https://caiji.maotaizy.cc/api.php/provide/vod',
+        name: '茅台资源',
     },
         ckzy: {
-        api: 'https://www.ckzy1.com',
-        name: 'CK资源',
+        api: 'https://api.souavzy.vip/api.php/provide/vod',
+        name: '搜AV',
         adult: true
     },
     jkun: {
-        api: 'https://jkunzyapi.com',
-        name: 'jkun资源',
+        api: 'https://apilsbzy1.com/api.php/provide/vod',
+        name: '老色逼',
         adult: true
     },
     bwzy: {
-        api: 'https://api.bwzym3u8.com',
+        api: 'https://api.bwzyz.com/api.php/provide/vod',
         name: '百万资源',
         adult: true
     },
     souav: {
-        api: 'https://api.souavzy.vip',
-        name: 'souav资源',
+        api: 'https://thzy1.me/api.php/provide/vod',
+        name: '桃花视频',
         adult: true
     },
     r155: {
-        api: 'https://155api.com',
-        name: '155资源',
+        api: 'http://fhapi9.com/api.php/provide/vod',
+        name: '番号资源',
         adult: true
     },
     lsb: {
-        api: 'https://apilsbzy1.com',
-        name: 'lsb资源',
+        api: 'https://91jpzyw.com/api.php/provide/vod',
+        name: '91资源',
         adult: true
     },
     huangcang: {
